7b25ed30266bd2391a6537ab34d1ba38617b1bfd
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-23 10:24:28 -0300

    remove b

--- index.py
+++ index.py
@@ -5,12 +5,8 @@ process = subprocess.Popen(['ss', '-ntu'],
                            universal_newlines=True)
 
 while True:
-    # output = process.stdout.readline()
-    # print(output.strip())
-    # # Do something else
     return_code = process.poll()
     if return_code is not None:
-        print('RETURN CODE', return_code)
         # Process has finished, read rest of the output 
         for output in process.stdout.readlines():
             print(output.strip())
