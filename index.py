59180e63b1550c9a16e97f646b666b02ed64548d
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-23 13:56:29 -0300

    other solution

--- index.py
+++ index.py
@@ -22,14 +22,23 @@ while True:
         for row in row_lists:
             ip = row[5].split(":")[0]
             if ip in ip_count:
-                ip_count[ip] += int(row[2])
+                ip_count[ip] += 1
             else:
-                ip_count[ip] = int(row[2])
+                ip_count[ip] = 1
+
+        print(ip + " --> Connections: " + str(ip_count[ip]) +"\n")
+
+        # for row in row_lists:
+        #     ip = row[5].split(":")[0]
+        #     if ip in ip_count:
+        #         ip_count[ip] += int(row[2])
+        #     else:
+        #         ip_count[ip] = int(row[2])
         
-        for ip in ip_count.keys():
-            if ip in ip_max_count:
-                if int(ip_count[ip]) > ip_max_count[ip]:
-                    ip_max_count[ip] = int(ip_count[ip])
-            else:
-                ip_max_count[ip] = int(ip_count[ip])
-            print(ip + " --> Actual: " + str(ip_count[ip]) + "  Max: " + str(ip_max_count[ip]) +"\n")
+        # for ip in ip_count.keys():
+        #     if ip in ip_max_count:
+        #         if int(ip_count[ip]) > ip_max_count[ip]:
+        #             ip_max_count[ip] = int(ip_count[ip])
+        #     else:
+        #         ip_max_count[ip] = int(ip_count[ip])
+        #     print(ip + " --> Actual: " + str(ip_count[ip]) + "  Max: " + str(ip_max_count[ip]) +"\n")
