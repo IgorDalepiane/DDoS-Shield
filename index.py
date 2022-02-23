2043f40fceabe5f2d4ac069d20d2b765776c4bf5
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-23 10:11:13 -0300

    index

--- index.py
+++ index.py
@@ -7,11 +7,3 @@ process = subprocess.Popen(['ss', '-ntu'],
 while True:
     output = process.stdout.readline()
     print(output.strip())
-    # Do something else
-    return_code = process.poll()
-    if return_code is not None:
-        print('RETURN CODE', return_code)
-        # Process has finished, read rest of the output 
-        for output in process.stdout.readlines():
-            print(output.strip())
-        break
