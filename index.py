b3fed6a36fa3de012fb4633a01885bcba188d558
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-23 11:04:13 -0300

    index

--- index.py
+++ index.py
@@ -11,17 +11,18 @@ while True:
 
     row_lists = []
     if process.returncode is not None:
-        rows = process.stdout.strip().split("\n")
-        print(rows)
-        for row in rows:
-            row_lists.append(row.split())
-        
-        row_lists.remove(row_lists[0])
-        print("IP" + " --> " + "Packages sent")
-        
-        for row in row_lists:
-            count += int(row[3])
-            print(row[5] + " --> " + row[3])
-            print(count)
+        for output in process.stdout:
+            rows = output.strip().split("\n")
+            print(rows)
+            for row in rows:
+                row_lists.append(row.split())
+            
+            row_lists.remove(row_lists[0])
+            print("IP" + " --> " + "Packages sent")
+            
+            for row in row_lists:
+                count += int(row[3])
+                print(row[5] + " --> " + row[3])
+                print(count)
 
         time.sleep(2)
