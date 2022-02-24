0705b02d5bb4cf2a57c2ed7148982b953e9c03c0
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-24 15:50:43 -0300

    print

--- index.py
+++ index.py
@@ -17,7 +17,6 @@ except:
     time.sleep(2)
 ip_ports={}
 while True:
-    print(ip_ports)
     ip_count = {}
     
     process = subprocess.run(['ss', '-ntu'], 
@@ -33,6 +32,7 @@ while True:
         tcp_cons.remove(tcp_cons[0])
 
         subprocess.run(['clear'])
+        print(ip_ports)
         temp_block=[]
         for row in tcp_cons:
             ip = row[5].split(":")[0]
