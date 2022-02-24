9755d9ded375441c43260ae5378e61010df832cf
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-24 11:14:55 -0300

    trying psutil

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import sys
+import psutil
 
 ip_bytes_count = {}
 ips_blocked = []
@@ -41,7 +42,7 @@ while True:
 
         elif sys.argv[1] == '-b':
             subprocess.run(['clear'])
-            
+            print(psutil.net_connections(kind='tcp'))
             print("IP" + " --> " + "Bytes sent")
             for row in tcp_cons:
                 ip = row[5].split(":")[0]
