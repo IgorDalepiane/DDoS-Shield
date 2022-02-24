1724a9c27897f4d375ab3c5db7aa8e678028af9b
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2022-02-24 14:13:33 -0300

    arguments

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ import sys
 
 ip_bytes_count = {}
 ips_blocked = []
+MAX_CONNECTIONS = 50
+MAX_SAME_BYTES_CONNECTIONS = 50
 while True:
     ip_count = {}
     
@@ -18,78 +20,74 @@ while True:
             tcp_cons.append(row.split())
         
         tcp_cons.remove(tcp_cons[0])
-        if len(sys.argv) == 1 or sys.argv[1] != '-c' and sys.argv[1] != '-b':
-            print("Invalid argument, use -c for connections or -b for bytes method.")
+
+        try:
+            MAX_CONNECTIONS = sys.argv[1]
+            MAX_SAME_BYTES_CONNECTIONS = sys.argv[2]
+        except:
+            print("If you want to specify a number of connections or \nconnections with same number of bytes:")
+            print("sudo index.py <MAX_CONNECTIONS> <MAX_SAME_BYTES_CONNECTIONS>")
+            print("Using default value (50) for both")
+            time.sleep(2)
+
+        if len(sys.argv) < 5:
+            print("Please use '-c <MAX_CONNECTIONS>'")
             exit(1)
-        elif sys.argv[1] == '-c':
-            if len(sys.argv) < 3:
-                print("Please use '-c <MAX_CONNECTIONS>'")
-                exit(1)
+        subprocess.run(['clear'])
+        for row in tcp_cons:
+            ip = row[5].split(":")[0]
+            ip_bytes = ip+":"+row[2]+":"+row[4].split(":")[1]
 
-            subprocess.run(['clear'])
-            print("IP" + " --> " + "Connections")
-            
-
-            for ip in ip_count.keys():
-                print(ip + " --> Connections: " + str(ip_count[ip]), end="\n")
-            time.sleep(1)
-
-        elif sys.argv[1] == '-b':
-            subprocess.run(['clear'])
-            for row in tcp_cons:
-                ip = row[5].split(":")[0]
-                ip_bytes = ip+":"+row[2]+":"+row[4].split(":")[1]
-
-                if int(row[2]) != 0:
-                    if ip_bytes in ip_bytes_count:
-                        ip_bytes_count[ip_bytes] += 1
-                    else:
-                        ip_bytes_count[ip_bytes] = 1
-
-                if ip in ip_count:
-                    ip_count[ip] += 1
+            if int(row[2]) != 0:
+                if ip_bytes in ip_bytes_count:
+                    ip_bytes_count[ip_bytes] += 1
                 else:
-                    ip_count[ip] = 1
+                    ip_bytes_count[ip_bytes] = 1
 
-            print("Number of Connections:", end='\n')
-            for ip in ip_count.keys():
-                print(ip + " --> " + str(ip_count[ip]), end="\n")
+            if ip in ip_count:
+                ip_count[ip] += 1
+            else:
+                ip_count[ip] = 1
 
-            print("\nIP" + " --> " + "Bytes sent")
-            temp_block=[]
-            for key in ip_bytes_count.keys():
-                ip = key.split(":")[0]
-                ip_bytes = key.split(":")[1]
+        print("Number of Connections:", end='\n')
+        for ip in ip_count.keys():
+            print(ip + " --> " + str(ip_count[ip]), end="\n")
+
+        print("\nIP" + " --> " + "Bytes sent")
+        temp_block=[]
+        for key in ip_bytes_count.keys():
+            ip = key.split(":")[0]
+            ip_bytes = key.split(":")[1]
+            
+            if ip_bytes_count[key] > 50:
+                ips_blocked.append(key)
                 
-                if ip_bytes_count[key] > 50:
-                    ips_blocked.append(key)
-                    
-                    subprocess.run(['sudo','iptables', '-A', 'INPUT', '-p', 'tcp', '--dport', key.split(":")[2], '-s', ip, '-j', 'DROP'], 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-                    subprocess.run(['sudo','ss', '-K', 'dst', ip], 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-                    temp_block.append(key)
+                subprocess.run(['sudo','iptables', '-A', 'INPUT', '-p', 'tcp', '--dport', key.split(":")[2], '-s', ip, '-j', 'DROP'], 
+                        stdout=subprocess.PIPE,
+                        universal_newlines=True)
+                subprocess.run(['sudo','ss', '-K', 'dst', ip], 
+                        stdout=subprocess.PIPE,
+                        universal_newlines=True)
+                temp_block.append(key)
 
-                print(ip + ":" + key.split(":")[2] + " --> Bytes: " + ip_bytes + " Count: " + str(ip_bytes_count[key]), end="\n")
-            
-            remove_other_tcp = []
-            for ip in temp_block:
-                ip_bytes_count.pop(ip)
-                for key in ip_bytes_count:
-                    ipstring_key = key.split(":")[0] + ":" + key.split(":")[2]
-                    ipstring_blocked = ip.split(":")[0] + ":" + ip.split(":")[2]
+            print(ip + ":" + key.split(":")[2] + " --> Bytes: " + ip_bytes + " Count: " + str(ip_bytes_count[key]), end="\n")
+        
+        remove_other_tcp = []
+        for ip in temp_block:
+            ip_bytes_count.pop(ip)
+            for key in ip_bytes_count:
+                ipstring_key = key.split(":")[0] + ":" + key.split(":")[2]
+                ipstring_blocked = ip.split(":")[0] + ":" + ip.split(":")[2]
 
-                    if ipstring_key == ipstring_blocked:
-                        remove_other_tcp.append(key)
+                if ipstring_key == ipstring_blocked:
+                    remove_other_tcp.append(key)
 
-            for tcp in remove_other_tcp:
-                ip_bytes_count.pop(tcp)
+        for tcp in remove_other_tcp:
+            ip_bytes_count.pop(tcp)
 
-            print("\nIPs Blocked:",end="\n")
-            for ipblocked in ips_blocked:
-                ipstring = ipblocked.split(":")[0] + ":" + ipblocked.split(":")[2]
-                print("BLOCKED: "+ipstring, end="\n")
-            
-            time.sleep(0.04)
+        print("\nIPs Blocked:",end="\n")
+        for ipblocked in ips_blocked:
+            ipstring = ipblocked.split(":")[0] + ":" + ipblocked.split(":")[2]
+            print("BLOCKED: "+ipstring, end="\n")
+        
+        time.sleep(0.04)
